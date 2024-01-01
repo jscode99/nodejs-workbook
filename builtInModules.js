405c4025a71fbb8faaf9114336c7b992e2b40a97
Jishnu <jscode99@gmail.com>
2024-01-01 13:23:28 +0530

    Built-In Modules v1.0.0

---
+++ builtInModules.js
@@ -0,0 +1,16 @@
+/**
+ * - Modules that Node.js ships with.
+ * 
+ * - Also referred to as Core modules.
+ * 
+ * - Import the module before you can use it.
+ * 
+ * - 5 Important built-in modules
+ * 
+ * 1) path
+ * 2) events
+ * 3) fs
+ * 4) stream
+ * 5) http
+ * 
+ */

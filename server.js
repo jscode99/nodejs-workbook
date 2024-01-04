0013944a1d5709aedf060924b547f973d60cd252
Jishnu <jscode99@gmail.com>
2024-01-04 09:42:55 +0530

    Built-In Module: HTTP v1.0.1 & Response HTML

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const http = require("node:http");
+const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: "This is your data!",
-    }),
-  );
+  // const batMan = {
+  //   firstName: "Bruce",
+  //   lastName: "Wayne",
+  // };
+  // res.writeHead(200, { "Content-Type": "application/json" });
+  // res.end(
+  //   JSON.stringify({
+  //     data: batMan,
+  //   }),
+  // );
+  res.writeHead(200, { "Content-Type": "text/html" });
+  // res.end(`<h1>Batman's real name is ${batMan.firstName} ${batMan.lastName}!!</h1>`);
+  const batManFilePath = __dirname + "/batman.html";
+  // const batManFile = fs.readFileSync(batManFilePath, "utf-8");
+  // res.end(batManFile);
+
+
+  /**
+   * ====================== Important Note ===================== 
+   * readFileSync will read the entire file at once. Will store the content to a temporary
+   * buffer which may lead to unnecessary use of memory. Instead we can rely on streams.
+   */
+  fs.createReadStream(batManFilePath).pipe(res);
 });
 
 server.listen(3000, () => {
